a7d9f70d21cc43b891e12f20ddbed12cf974ff0e
onggiabayluon <livipst2@gmail.com>
2022-06-29 08:25:21 +0700

    6️⃣3️⃣: Dựng khung Layout mặc định

--- src/components/Layout/components/Sidebar/Sidebar.js
+++ src/components/Layout/components/Sidebar/Sidebar.js
@@ -1,5 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <aside>🗽 Sidebar</aside>;
+    return <aside className={cx('wrapper')}>🗽 Sidebar</aside>;
 }
 
 export default Sidebar;
