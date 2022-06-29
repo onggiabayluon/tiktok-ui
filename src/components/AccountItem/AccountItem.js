b74a02151d6be7ccc5330ed5de848de9f1cc05d1
onggiabayluon <livipst2@gmail.com>
2022-06-29 16:47:59 +0700

    6️⃣5️⃣: Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img src="https://i.imgur.com/rJSYoaa.jpeg" alt="Dog" className={cx('avatar')} />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
