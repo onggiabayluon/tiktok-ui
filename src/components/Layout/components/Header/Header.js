b9463d116da1b00022e53fac175bfb216cae4c00
Duc Huy <livipst2@gmail.com>
2022-07-11 16:40:51 +0700

    6️⃣7️⃣: Xây dựng UI phần Header (Btn, Menu)#4

--- src/components/Layout/components/Header/Header.js
+++ src/components/Layout/components/Header/Header.js
@@ -2,7 +2,28 @@ import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Search from '~/components/Layout/components/Search';
+import Menu from '~/components/Popper/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEarthAsia, faEllipsisVertical, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons';
+import Button from '~/components/Button';
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'FeedBack and Help',
+        to: '/feedback',
+    },
+    {
+        // Show Modals
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
 
 function Header() {
     return (
@@ -16,8 +37,16 @@ function Header() {
                 <Search />
 
                 <div className={cx('action-list')}>
-                    <div className={cx('action')}>💟</div>
-                    <div className={cx('action')}>🔏</div>
+                    <div className={cx('actions')}>
+                        <Button text>Upload</Button>
+                        <Button primary>Log in</Button>
+                    </div>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
